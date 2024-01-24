7d3de0b12e5fa289e584a1aa92ad3a406e1484cf
Rizki Firmansyah <rizfirman@gmail.com>
2024-01-24 21:25:51 +0700

    add new route for detail product

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -4,35 +4,44 @@ import Product from '../models/Product.js';
 const productRoutes = express.Router();
 
 const getProducts = async (req, res) => {
-
-  const page = parseInt(req.params.page)
-  const perPage = parseInt(req.params.perPage)
-
+  const page = parseInt(req.params.page);
+  const perPage = parseInt(req.params.perPage);
 
   const products = await Product.find({});
- 
-  if(page && perPage){
-    const totalPages = Math.ceil(products.length / perPage)
-    const startIndex = (page - 1) * perPage
-    const endIndex = startIndex + perPage
-    const paginatedProducts = products.slice(startIndex, endIndex)
+
+  if (page && perPage) {
+    const totalPages = Math.ceil(products.length / perPage);
+    const startIndex = (page - 1) * perPage;
+    const endIndex = startIndex + perPage;
+    const paginatedProducts = products.slice(startIndex, endIndex);
     res.json({
-      products:paginatedProducts,
-      pagination:{
+      products: paginatedProducts,
+      pagination: {
         totalPages,
-        currentPage:page,
-      }
+        currentPage: page,
+      },
     });
   } else {
     res.json({
       products,
-      pagination:{}
+      pagination: {},
     });
+  }
+};
+
+const getProduct = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+
+    throw new Error('Product not found');
   }
 };
 
 productRoutes.route('/:page/:perPage').get(getProducts);
 productRoutes.route('/').get(getProducts);
-
+productRoutes.route('/:id').get(getProduct);
 
 export default productRoutes;
